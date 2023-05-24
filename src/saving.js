9e1c9a6e9a3088a58060d84da441c7db72d7e89a
Owen Wheary <owen356wh@gmail.com>
2023-05-23 19:59:03 -0700

    added document serialization

--- src/saving.js
+++ src/saving.js
@@ -1,11 +1,17 @@
 function saveDocument() {
-  console.log("saving...");
+  const regexp = /.*?\>(.*)\<\/.*/g; // accursed regex to extract children
+  const serializer = new XMLSerializer(); // for serializing DOM objects to text
 
   const doc = document.getElementById("document");
-  console.log(doc.innerText);
+  const articleHTML = serializer.serializeToString(doc);
 
+  // removes the outer <article> tag from string
+  const fileHTML = [...articleHTML.matchAll(regexp)][0][1];
+
+  // this is a hack to download files, by adding a download link to the DOM,
+  // clicking it through JS, and then removing it. It works on firefox though...
   var a = window.document.createElement("a");
-  a.href = window.URL.createObjectURL(new Blob([doc.innerText]), {
+  a.href = window.URL.createObjectURL(new Blob([fileHTML]), {
     type: "test/plain",
   });
   a.download = "your_file.txt";
