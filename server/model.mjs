24b1df129058d3268941f7c43df3e3adca31e8b8
Owen Wheary <owen356wh@gmail.com>
2023-06-05 21:47:32 -0700

    Refactored server to work w/ a mongodb model, added .env (currently ignored for development purposes), added the beginning of a database model.

---
+++ server/model.mjs
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import "dotenv/config";
+
+mongoose.connect(process.env.MONGODB_CONNECT_STRING, {
+  useNewUrlParser: true,
+});
+
+const db = mongoose.connection;
+
+const documentSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+});
+
+const Document = mongoose.model("Document", documentSchema);
